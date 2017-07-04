202d95f934c83d8fea07d6e864efb470bdcd016f
WanShichen <wanshichen@xindong.com>
2017-07-04 22:29:58 +0800

    delete GameManagers\ auto\ func

--- Assets/Codes/GameManager.cs
+++ Assets/Codes/GameManager.cs
@@ -2,15 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour {
+public class GameManager : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
