ddecb00913bb8b497b7eee005828ca5d60eddbbc
WanShichen <wanshichen@xindong.com>
2017-07-04 21:48:01 +0800

    add STL DictionaryWithListKey

---
+++ Assets/Plugins/STL/DictionaryWithListKey.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+
+namespace Wsc.STL
+{
+    public class DictionaryWithListKey<TKey, TValue>
+    {
+        private DictionaryWithListKey()
+        {
+            keys = new List<TKey>();
+            dict = new Dictionary<TKey, TValue>();
+        }
+        public DictionaryWithListKey(TValue defaultValue) : this()
+        {
+            this.defaultValue = defaultValue;
+        }
+        TValue defaultValue;
+        List<TKey> keys;
+        Dictionary<TKey, TValue> dict;
+
+        public void Set(TKey key, TValue value)
+        {
+            if (!dict.ContainsKey(key))
+            {
+                keys.Add(key);
+            }
+            dict[key] = value;
+        }
+
+        public void SetAll(TValue value)
+        {
+            for (int i = keys.Count - 1; i >= 0; i--)
+            {
+                dict[keys[i]] = value;
+            }
+        }
+
+        public TValue Get(TKey key)
+        {
+            if (!dict.ContainsKey(key))
+            {
+                return defaultValue;
+            }
+            return dict[key];
+        }
+    }
+}
