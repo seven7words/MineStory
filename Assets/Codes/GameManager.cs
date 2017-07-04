6acc0bb3ed960e7715ea8c08b8955c4005be9fb9
WanShichen <wanshichen@xindong.com>
2017-07-05 00:47:18 +0800

    create BehaviourManager and InputManager

--- Assets/Codes/GameManager.cs
+++ Assets/Codes/GameManager.cs
@@ -1,8 +1,23 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Wsc.Behaviour;
+using Wsc.Input;
 
 public class GameManager : MonoBehaviour
 {
+    void Awake()
+    {
+        Init();
+    }
 
+    private BehaviourManager behaviourManager;
+    private InputManager inputManager;
+
+    private void Init()
+    {
+        behaviourManager = new BehaviourManager();
+        inputManager = new InputManager(behaviourManager);
+    }
 }
